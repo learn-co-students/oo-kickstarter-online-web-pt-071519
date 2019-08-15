cfb3cfd9ae99ace59eb503fb7528c2dd4f749f05
LaNice Powell <lanicemp@aol.com>
2019-08-15 10:54:42 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project  
+  
+  attr_reader :title, :backers
+  #attr_accessor :backed_projects
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self 
+  end 
+end 
